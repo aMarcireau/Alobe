32123c1c4ade3ac1031d79428b0f15ca9c42a38a
GepNX <clement.ricateau@hotmail.fr>
2014-10-13 19:08:44 +0200

    Resolve main conflict

--- alobe/main.cpp
+++ alobe/main.cpp
@@ -1,13 +1,36 @@
 #include "simulation.h"
 #include "stepper.h"
 #include "actor.h"
+#include "Land.h"
+#include "Tile.h"
+#include <iostream>
+#include <memory>
 
 using namespace std;
 
-int main() {
+int main()
+{
+
     unique_ptr<Simulation> simulation = make_unique<Simulation>(make_unique<Stepper>());
+	int height, width;
 
-    simulation->toStep(10);
+	cout << "Enter a height for your land: " << endl;
+	cin >> height;
+	cout << "Enter a width for your land: " << endl;
+	cin >> width;
+	Land land(height, width);
+	cout << "the land width is " << land.getWidth() << endl;
+	cout << "The land's height is " << land.getHeight() << endl;
+	cout << "The number of tiles in this land is  " << land.getTilesNumber() << endl;
+	cout << "Filling of the land with tiles! " << endl;
+	land.generate();
+	cout << "The land is filled! " << endl;
+	
+	for (map<vector<int>, unique_ptr<Tile> >::iterator it = land.tileLand.begin(); it != land.tileLand.end(); ++it){
+		Tile * tileTest = (it->second).get();
+		cout << (*tileTest).confirm() << '\n';
+	}
 
-    return 0;
+	simulation->toStep(10);
+	return 0;
 }
