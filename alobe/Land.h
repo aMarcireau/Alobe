e66fef58a50521aa321597a171c924f48bafd025
Alexandre Marcireau <alexandre.marcireau@student.ecp.fr>
2014-10-19 23:15:51 +0200

    Refactor Land to use a 2d vector of unique_ptr

--- alobe/Land.h
+++ alobe/Land.h
@@ -1,23 +1,35 @@
+#ifndef __ALOBE__LAND__
+#define __ALOBE__LAND__
+
 #include <iostream>
 #include <vector>
-#include <map>
 #include <memory>
-#include "Tile.h"
+
+#include "actor.h"
+#include "tile.h"
+
 using namespace std;
 
-class Land
+/**
+ * Land
+ * Inherits Actor
+ *
+ * Land of the simulation
+ */
+class Land: Actor
 {
-public:
-	Land(int width, int height);
-	int getWidth() const;
-	int getHeight() const;
-	Tile * getTile(int x, int y);
+    public:
+        Land(unsigned int width, unsigned int height);
+        unsigned int getWidth() const;                        // Getter for the land width (number of tiles)
+        unsigned int getHeight() const;                       // Getter for the land height (number of tiles)
+        Tile * getTile(unsigned int x, unsigned int y) const; // Get tile by position
+        unsigned int getTilesNumber() const;                  // Get the number of tiles
+        void generate();                                      // Generate the land tiles
 
-	int getTilesNumber() const;
-	void generate();
 
-public:
-	int width, height;
-	map<vector<int>, unique_ptr<Tile> > tileLand;
+    private:
+        unsigned int _width, _height;
+        vector<vector<unique_ptr<Tile> > > _tiles;
 };
 
+#endif // __ALOBE__LAND__
