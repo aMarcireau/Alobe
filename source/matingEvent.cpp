9ca14b2a2811c776c21dc862aba17276cce8763b
Alexandre Marcireau <alexandre.marcireau@student.ecp.fr>
2014-11-04 00:39:53 +0100

    Use getTiles

--- source/matingEvent.cpp
+++ source/matingEvent.cpp
@@ -14,34 +14,33 @@ MatingEvent::MatingEvent() :
 void MatingEvent::filteredAction(Actor & actor)
 {
 	Population & population(dynamic_cast<Population &>(actor));
-	// Do action on all the tiles
+
 	for (unsigned long x = 0; x < population.getLand()->getWidth(); ++x) {
 		for (unsigned long y = 0; y < population.getLand()->getHeight(); ++y) {
-			map<string, Tile *> neighboringTiles = population.getLand()->getNeighboringTiles(x, y);
-			// Get all the beings on a tile
-			vector<Being *> beings = neighboringTiles["here"]->getBeings();
-			// Iteration on all beings
+
+			vector<Being *> beings = population.getLand()->getTile(x, y)->getBeings();
+
 			for (
 				vector<Being *>::iterator beingIterator = beings.begin();
 				beingIterator != beings.end();
 				++beingIterator
-				) {
+            ) {
 				//Iteration on all beings that haven't met the previous being yet
 				for (
-					vector<Being *>::iterator otherBeingsIterator = beingIterator;
+					vector<Being *>::iterator otherBeingsIterator = beingIterator + 1;
 					otherBeingsIterator != beings.end();
 					++otherBeingsIterator
-					){	
+                ){
 					//Test if the being want to mate with an other being
 					if ((*beingIterator)->isReadyToMate(**otherBeingsIterator))
-						{
-							// Creation of the child chromosomes
-							auto childChromosomes = (*beingIterator)->setChildChromosomes(**otherBeingsIterator);
-							//Creation of the new being on the current tile
-							population.addBeing(make_unique<Being>(to_string(rand()), childChromosomes), x, y);
-						}
-					}
-				}
-			}
-		}
-	}
+                    {
+                        // Creation of the child chromosomes
+                        vector<shared_ptr<Chromosome>> childChromosomes = (*beingIterator)->setChildChromosomes(**otherBeingsIterator);
+                        //Creation of the new being on the current tile
+                        population.addBeing(make_unique<Being>(to_string(rand()), childChromosomes), x, y);
+                    }
+                }
+            }
+        }
+    }
+}
