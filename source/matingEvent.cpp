876a1c2d9308b04e2881a010562ac450de57bdb4
Alexandre Marcireau <alexandre.marcireau@student.ecp.fr>
2014-11-15 17:49:57 +0100

    Refactor mating event

--- source/matingEvent.cpp
+++ source/matingEvent.cpp
@@ -13,39 +13,31 @@ MatingEvent::MatingEvent():
 */
 void MatingEvent::filteredAction(Actor & actor)
 {
-    /* TODO: REFACTOR WITH BEING BEHAVIOURS
-
 	Population & population(dynamic_cast<Population &>(actor));
-
-
 
 	for (unsigned long x = 0; x < population.getLand()->getWidth(); ++x) {
 		for (unsigned long y = 0; y < population.getLand()->getHeight(); ++y) {
 
 			vector<Being *> beings = population.getLand()->getTile(x, y)->getBeings();
-
+            unsigned long beingsNumber = beings.size();
 			for (
-				vector<Being *>::iterator beingIterator = beings.begin();
-				beingIterator != beings.end();
-				++beingIterator
+                unsigned long beingIndex = 0;
+                beingIndex < beingsNumber;
+                ++beingIndex
             ) {
-				//Iteration on all beings that haven't met the previous being yet
-				for (
-					vector<Being *>::iterator otherBeingsIterator = beingIterator + 1;
-					otherBeingsIterator != beings.end();
-					++otherBeingsIterator
+                for (
+                    unsigned long otherBeingIndex = beingIndex + 1;
+                    otherBeingIndex < beingsNumber;
+                    ++otherBeingIndex
                 ) {
-					//Test if the being want to mate with an other being
-					if ((*beingIterator)->isReadyToMate(**otherBeingsIterator))
-                    {
-                        // Creation of the child chromosomes
-                        vector<shared_ptr<Chromosome>> childChromosomes = (*beingIterator)->setChildChromosomes(**otherBeingsIterator);
-                        //Creation of the new being on the current tile
-                        population.addBeing(make_unique<Being>(to_string(rand()), childChromosomes), x, y);
+					if (beings[beingIndex]->isWillingToMateWith(*beings[otherBeingIndex]) and beings[otherBeingIndex]->isWillingToMateWith(*beings[beingIndex])) {
+                        population.addBeing(
+                            population.getBeingFactory()->generateBeing(*beings[beingIndex], *beings[otherBeingIndex]),
+                            x, y
+                        );
                     }
                 }
             }
         }
     }
-    */
 }
