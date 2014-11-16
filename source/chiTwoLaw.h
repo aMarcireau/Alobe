b3486ff7dd03d9b9bedba0fd009986d5f440518e
Xavier <xajouppe@gmail.com>
2014-11-16 15:31:28 +0100

    chi 2 law

---
+++ source/chiTwoLaw.h
@@ -0,0 +1,14 @@
+//
+//  chiTwoLaw.h
+//  alobe
+//
+//  Created by Xavier J on 16/11/2014.
+//  Copyright (c) 2014 Xavier. All rights reserved.
+//
+
+#ifndef __alobe__chiTwoLaw__
+#define __alobe__chiTwoLaw__
+
+#include <stdio.h>
+
+#endif /* defined(__alobe__chiTwoLaw__) */
