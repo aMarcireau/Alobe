bc14ef80ee69b8fac5adbbc0c0f206cf94835328
Alexandre Marcireau <alexandre.marcireau@student.ecp.fr>
2014-12-12 17:19:15 +0100

    Add smoothed move utility

--- source/utilities.cpp
+++ source/utilities.cpp
@@ -1,3 +1,4 @@
+
 #include "utilities.h"
 
 /**
@@ -8,19 +9,16 @@ long long llModulo(long long dividend, long long divisor){
 }
 
 /**
- * Calculate the coordinates of the 'position' element in the interval [minimum, maximum] divided by 'total' elements
+ * Calculate the coordinates of the 'position' element in the interval [minimum, minimum + length] divided by 'total' elements
  */
-unsigned long intervalToCoordinate(unsigned long minimum, unsigned long maximum, unsigned long total, unsigned long position
+unsigned long intervalToCoordinate(unsigned long minimum, unsigned long length, unsigned long total, unsigned long position
 )
 {
-    if (minimum > maximum) {
-        throw logic_error("Minimum is larger than maximum");
-    }
     if (position > total) {
-        throw logic_error("Position is larger than total");
+        throw out_of_range("total");
     }
 
-    return minimum + (unsigned long)(((double)(maximum - minimum)) / total * position);
+    return minimum + (unsigned long)((double)(length) / total * position);
 }
 
 /**
@@ -29,4 +27,55 @@ unsigned long intervalToCoordinate(unsigned long minimum, unsigned long maximum,
 vector<unsigned long> hexadecimalToRGB(unsigned long color)
 {
     return {color / 0x10000, (color / 0x100) % 0x100, color % 0x100};
+}
+
+
+/**
+ * Smoothed move next step
+ */
+unsigned long smoothedMove(unsigned long position, long long speed, unsigned long minimum, unsigned long maximum, float turnover)
+{
+    if (position < minimum or position > maximum) {
+        throw out_of_range("position");
+    }
+    if (turnover < 0 or turnover > 1) {
+        throw out_of_range("turnover");
+    }
+
+    long long newSpeed;
+    if (position == minimum) {
+        if (rand() % 2 == 0) {
+            newSpeed = 0;
+        } else {
+            newSpeed = 1;
+        }
+    } else if (position == maximum) {
+        if (rand() % 2 == 0) {
+            newSpeed = -1;
+        } else {
+            newSpeed = 0;
+        }
+    } else {
+        if (rand() < turnover * RAND_MAX) {
+            if (speed > 0 or speed < 0) {
+                newSpeed = 0;
+            } else {
+                if (rand() % 2 == 0) {
+                    newSpeed = -1;
+                } else {
+                    newSpeed = 1;
+                }
+            }
+        } else {
+            if (speed < 0) {
+                newSpeed = -1;
+            } else if (speed > 0) {
+                newSpeed = 1;
+            } else {
+                newSpeed = 0;
+            }
+        }
+    }
+
+    return position + newSpeed;
 }
