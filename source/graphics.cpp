6b6a1a2e562336e3b97b87c32e4c920a10dcf5db
Alexandre Marcireau <alexandre.marcireau@student.ecp.fr>
2014-12-12 17:20:45 +0100

    Add speed to graphics

--- source/graphics.cpp
+++ source/graphics.cpp
@@ -6,7 +6,9 @@
 Graphics::Graphics(shared_ptr<GraphicsWindow> graphicsWindow):
     my_graphicsWindow(graphicsWindow),
     my_xOffset(0),
-    my_yOffset(0)
+    my_yOffset(0),
+    my_width(graphicsWindow->getWidth()),
+    my_height(graphicsWindow->getHeight())
 {
 }
 
@@ -46,6 +48,7 @@ unsigned long Graphics::getYOffset()
  */
 void Graphics::setXOffset(unsigned long xOffset)
 {
+    my_xSpeed = (long long)(xOffset) - (long long)(my_xOffset);
     my_xOffset = xOffset;
 }
 
@@ -54,5 +57,54 @@ void Graphics::setXOffset(unsigned long xOffset)
  */
 void Graphics::setYOffset(unsigned long yOffset)
 {
+    my_ySpeed = (long long)(yOffset) - (long long)(my_yOffset);
     my_yOffset = yOffset;
+}
+
+/**
+ * Getter for the x offset
+ */
+long long Graphics::getXSpeed()
+{
+    return my_xSpeed;
+}
+
+/**
+ * Getter for the y offset
+ */
+long long Graphics::getYSpeed()
+{
+    return my_ySpeed;
+}
+
+/**
+ * Getter for the width
+ */
+unsigned long Graphics::getWidth()
+{
+    return my_width;
+}
+
+/**
+ * Getter for the height
+ */
+unsigned long Graphics::getHeight()
+{
+    return my_height;
+}
+
+/**
+ * Setter for the width
+ */
+void Graphics::setWidth(unsigned long width)
+{
+    my_width = width;
+}
+
+/**
+ * Setter for the y offset
+ */
+void Graphics::setHeight(unsigned long height)
+{
+    my_height = height;
 }
